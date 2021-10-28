4ddae5c37eee7be58eec00260ac72c633bfdd85c
hellojeongso <84715469+hellojeongso@users.noreply.github.com>
2021-10-29 04:46:25 +0900

    Js, CSS: about__right update

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,35 @@ var title = {
 }; 
   
 var typed = new Typed('.typed', title);
+
+
+var $mePrev = $('.about-btn__set>.prev');
+var $meNext = $('.about-btn__set>.next');
+var $aboutme = $('.about__profile>ul>li');
+var nowIdx = 0;
+
+//about me
+$mePrev.on('click',function(){
+  if(nowIdx>0){
+    nowIdx--;
+    $mePrev.addClass('on');
+  }else{
+    $mePrev.removeClass('on');
+    $meNext.addClass('on');
+  }
+
+  $aboutme.eq(nowIdx).fadeIn().siblings().fadeOut();
+});
+
+$meNext.on('click',function(){
+  if(nowIdx<2){
+    nowIdx++;
+    $meNext.addClass('on');
+  }else{
+    nowIdx = 2;
+    $meNext.removeClass('on');
+    $mePrev.addClass('on');
+  }
+
+  $aboutme.eq(nowIdx).fadeIn().siblings().fadeOut();
+});
